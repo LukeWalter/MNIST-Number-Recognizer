4bafe8e9a356009d7744c2016827e44d8a5f9c1a
Luke Walter <59621677+LukeWalter@users.noreply.github.com>
2025-08-31 10:21:24 -0400

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -1,81 +1,72 @@
 import numpy as np
 import idx2numpy
 
-def sigmoid(n):
-    ex = np.pow(np.e, n)
-    return ex / (1 + ex)
+class NeuralNet:
 
-# Training / testing data from MNIST
-train_images = idx2numpy.convert_from_file('train-images.idx3-ubyte')
-train_labels = idx2numpy.convert_from_file('train-labels.idx1-ubyte')
-test_images = idx2numpy.convert_from_file('t10k-images.idx3-ubyte')
-test_labels = idx2numpy.convert_from_file('t10k-labels.idx1-ubyte')
+    def sigmoid(n):
+        ex = np.pow(np.e, n)
+        return ex / (1 + ex)
 
-in_size = 784
-hl_size = 10
-hl_count = 3
-out_size = 5
+    def __init__(self, in_size, hl_size, hl_count, out_size):
+        
+        self.in_size = in_size
+        self.hl_size = hl_size
+        self.hl_count = hl_count
+        self.out_size = out_size
+        
+        self.hidden_layers = np.zeros((2, self.hl_count, self.hl_size), dtype=float)
 
-input_data = np.array([(train_images[0] / 255).flatten()])
+        self.in_to_hl_weights = np.random.uniform(low=-1, high=1, size=(self.in_size, self.hl_size))
+        self.hl_weight_rows = (self.hl_count - 1) * self.hl_size + self.out_size
+        self.hl_weights = np.random.uniform(low=-1, high=1, size=(self.hl_weight_rows, self.hl_size))
+        self.out_biases = np.zeros((1, self.out_size), dtype=float)
+        self.out_data = np.zeros((1, self.out_size), dtype=float)
+    
+    def train(self):
+        # Training / testing data from MNIST
+        train_images = idx2numpy.convert_from_file('train-images.idx3-ubyte')
+        train_labels = idx2numpy.convert_from_file('train-labels.idx1-ubyte')
+        test_images = idx2numpy.convert_from_file('t10k-images.idx3-ubyte')
+        test_labels = idx2numpy.convert_from_file('t10k-labels.idx1-ubyte')
 
-hidden_layers = np.zeros((2, hl_count, hl_size), dtype=float)
+        # Forward propagation
 
-in_to_hl_weights = np.random.uniform(low=-1, high=1, size=(in_size, hl_size))
-hl_weight_rows = (hl_count - 1) * hl_size + out_size
-hl_weights = np.random.uniform(low=-1, high=1, size=(hl_weight_rows, hl_size))
-# print(hl_weights.shape)
+        # Add a check here to see if the input data has the correct shape (based on in_size)
+        input_data = np.array([(train_images[0] / 255).flatten()])
 
-# Forward propagation
+        self.hidden_layers[0, 0] = NeuralNet.sigmoid(input_data @ self.in_to_hl_weights + self.hidden_layers[1, 0])
 
-hidden_layers[0, 0] = sigmoid(input_data @ in_to_hl_weights + hidden_layers[1, 0])
+        for i in range(self.hl_count - 1):
+            self.hidden_layers[0, i+1] = NeuralNet.sigmoid(self.hidden_layers[0, 0] @ self.hl_weights[i*self.hl_size:(i+1)*self.hl_size] + self.hidden_layers[1, (i+1)])
 
-for i in range(hl_count - 1):
-    hidden_layers[0, i+1] = sigmoid(hidden_layers[0, 0] @ hl_weights[i*hl_size:(i+1)*hl_size] + hidden_layers[1, (i+1)])
+        out_data = NeuralNet.sigmoid(np.array([self.hidden_layers[0, self.hl_count - 1]]) @ self.hl_weights[(self.hl_count-1)*self.hl_size:(self.hl_count-1)*self.hl_size+self.out_size].T + self.out_biases)
 
-out_biases = np.zeros((1, out_size), dtype=float)
-out_data = sigmoid(np.array([hidden_layers[0, hl_count - 1]]) @ hl_weights[(hl_count-1)*hl_size:(hl_count-1)*hl_size+out_size].T + out_biases)
-# print(out_data)
-print(out_biases)
+        # Back propagation
 
-# Back propagation
-learn_rate = -0.01
+        learn_rate = -0.01
 
-out_ideal = np.zeros(out_data.shape, dtype=float)
-out_ideal[0, train_labels[1]] = 1
+        out_ideal = np.zeros(out_data.shape, dtype=float)
+        out_ideal[0, train_labels[1]] = 1
 
-# output layer to hidden layers
-error = out_ideal - out_data
-# hl_weights[(hl_count-1)*hl_size:(hl_count-1)*hl_size+out_size] -= learn_rate * (np.array([hidden_layers[0, 2]]).T @ error).T
-hl_weights[20:30] -= learn_rate * (np.array([hidden_layers[0, 2]]).T @ error).T
-out_biases -= learn_rate * error
-h = hidden_layers[0, 2]
-derivative = np.array([h * (1 - h)])
-# delta_h = error @ hl_weights[(hl_count-1)*hl_size:(hl_count-1)*hl_size+out_size] * derivative
-delta_h = error @ hl_weights[20:30] * derivative
-# print(delta_h[0])
+        error = out_ideal - out_data
+        self.hl_weights[(self.hl_count-1)*self.hl_size:(self.hl_count-1)*self.hl_size+self.out_size] -= learn_rate * (np.array([self.hidden_layers[0, self.hl_count - 1]]).T @ error).T
+        self.out_biases -= learn_rate * error
+        h = self.hidden_layers[0, self.hl_count - 1]
+        derivative = np.array([h * (1 - h)])
+        delta_h = error @ self.hl_weights[(self.hl_count-1)*self.hl_size:(self.hl_count-1)*self.hl_size+self.out_size] * derivative
 
-# hidden layers
-hl_weights[10:20] -= learn_rate * (np.array([hidden_layers[0, 1]]).T @ delta_h).T
-# print(hidden_layers[1, 2])
-hidden_layers[1, 2] -= learn_rate * delta_h[0]
-h = hidden_layers[0, 1]
-derivative = derivative = np.array([h * (1 - h)])
-delta_h = delta_h @ hl_weights[10:20] * derivative
-# print(delta_h)
-# print(hidden_layers[1, 2])
+        for i in range(self.hl_count - 1):
+            
+            layer = (self.hl_count - 2) - i
+            
+            self.hl_weights[layer*self.hl_size:(layer+1)*self.hl_size] -= learn_rate * (np.array([self.hidden_layers[0, layer]]).T @ delta_h).T
+            self.hidden_layers[1, (layer+1)] -= learn_rate * delta_h[0]
+            h = self.hidden_layers[0, layer]
+            derivative = np.array([h * (1 - h)])
+            delta_h = delta_h @ self.hl_weights[layer*self.hl_size:(layer+1)*self.hl_size] * derivative
 
-hl_weights[0:10] -= learn_rate * (np.array([hidden_layers[0, 0]]).T @ delta_h).T
-hidden_layers[1, 1] -=  learn_rate * delta_h[0]
-h = hidden_layers[0, 0]
-derivative = np.array([h * (1 - h)])
-delta_h = delta_h @ hl_weights[0:10] * derivative
-# print(delta_h)
-
-# hidden layers to input layer
-# print(input_data)
-# print(input_data.shape)
-# print((input_data.T @ delta_h).shape)
-# print(in_to_hl_weights.shape)
-print(in_to_hl_weights)
-in_to_hl_weights -= 1000 * input_data.T @ delta_h
-print(in_to_hl_weights)
+        self.in_to_hl_weights -= learn_rate * input_data.T @ delta_h
+        self.hidden_layers[1, 0] -=  learn_rate * delta_h[0]
+        
+nn = NeuralNet(784, 10, 3, 10)
+nn.train()
